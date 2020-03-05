3cb2f71709c78ad5ec365d0201f7e73e98808b31
Wang Liang <nkhs@yandex.com>
2020-03-06 05:19:06 +0800

    dota hook with firebase db

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { Const } from './data';
+
+
+declare let io: any;
+declare let firebase: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  socket: any;
+  initSocket() {
+    this.socket = io("https://localhost");
+    this.socket.on('connect', () => {
+      console.log('connected');
+    })
+    this.socket.on('disconnect', () => {
+      console.log('disconnected');
+    })
 
+
+    this.socket.on('chat message', (data) => this.onSocketDataChatMessage(data));
+  }
+  log = ["test"]
+  onSocketDataChatMessage(data) {
+    console.error(data);
+  }
 
   data: any[] = Const.data.reverse();
   dataDisplay: any[] = []
@@ -40,7 +61,45 @@ export class AppComponent implements OnInit {
       if (ofName.toLowerCase() == baseItem.name.toLowerCase() || ofName.toLowerCase().includes(baseItem.name.toLowerCase()) || baseItem.name.toLowerCase().includes(ofName.toLowerCase())) return baseItem;
     }
   }
+
+  COMPUTER = "DESKTOP-93C0J9U";
+
+  initFirebase() {
+    var config = {
+      apiKey: "AIzaSyAxHtLArlYkXIFI2PStYIqEsHZJ7qa6hNg",
+      authDomain: "dota-item.firebaseapp.com",
+      databaseURL: "https://dota-item.firebaseio.com",
+      projectId: "dota-item",
+      storageBucket: "dota-item.appspot.com",
+      messagingSenderId: "434749815159",
+      appId: "1:434749815159:web:e6ea59c6b7f5a245508972",
+      measurementId: "G-S92E5G11KR"
+    };
+    firebase.initializeApp(config);
+
+    var commentsRef = firebase.database().ref(`dota/${this.COMPUTER}/`);
+
+    commentsRef.on('child_added', (data) => {
+      console.log(data.key, data.val())
+      this.search = data.val();
+      this.onChangeSearch();
+    });
+
+    // var commentsRef = firebase.database().ref(`dota/${this.COMPUTER}/`);
+    // commentsRef.once('value', function (snapshot) {
+    //   snapshot.forEach(function (childSnapshot) {
+    //     var childKey = childSnapshot.key;
+    //     var childData = childSnapshot.val();
+    //     console.log(childKey, childData)
+
+    //   });
+    // });
+
+  }
   ngOnInit(): void {
+    this.initFirebase();
+    // this.initSocket();
+
     for (var i = 0; i < this.base.length; i++) {
       this.base[i].img = `assets/${this.base[i].img}`
     }
